0986a4cc0c3feffee6fdbf190879a1efb142af60
mburakbilgic <muhammed.burak.bilgic@gmail.com>
2021-09-25 00:13:46 +0300

    feat/fix(updates):new logical changes implemented

--- wp_automsg/wp_automsg_script.py
+++ wp_automsg/wp_automsg_script.py
@@ -3,41 +3,25 @@ import pyautogui
 import time
 import webbrowser
 import argparse
-import os
-import pandas as pd
 from time import sleep
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..','..'))
+
+from wp_batch.wp_message_batch import MessageBatch
 
 class AutoMessage:
     def __init__(self):
-        self.current_path = os.getcwd()
-        print(self.current_path)
-        #TODO:needs to be autocapture for path files, data needs to be importable, research for pathlib or relevant libraries
-        self._data = pd.read_csv("C:/Users/muham/Desktop/wpchallenge/wp_automsg/phonebook.csv", sep=',',header=0)
-        self.arr_name = []
-        self.arr_pnumber = []
-        self.arr_msg = []
-        self.batch = {}
-
-    def data(self):
-        for idx in self._data.index:
-            pnumber = self._data.loc[idx,'phonenumber']
-            name = self._data.loc[idx,'name']
-            
-            self.arr_pnumber.append(str(pnumber))
-            self.arr_name.append(name)
-
-    def message_batch(self):
-        #TODO:batch needs to be improve
-        for idx_name in self.arr_name:
-            msg='Hi %s! my new phone number is 5341903034, please remove the other. Thank you!' %idx_name
-            self.arr_msg.append(msg)
-
-        self.batch = dict(zip(self.arr_pnumber,self.arr_msg))
+        self.image_path = '%s/wp_images/' %os.path.dirname(os.path.realpath(__file__)).replace('\\','/')
 
     def message_send_wp(self):
-        #TODO: input needed for receiver define logics, also additional variables be needed for it in the data
-        open_wp_image_path = '%s\open_whatsapp.png' %self.current_path
-        click_sent_image_path = '%s\click_sent.png' %self.current_path
+        #TODO: input needed for schedule time
+        unique_batch = MessageBatch().msg_unique_batch()
+        batch = MessageBatch().msg_batch()
+        
+        open_wp_image_path = '%s/open_whatsapp.png' %self.image_path
+        click_sent_image_path = '%s/click_sent.png' %self.image_path
 
         for _pnumber,_msg in self.batch.items():
             webbrowser.open_new_tab(f'https://api.whatsapp.com/send?phone=+90{_pnumber}&text={_msg}')
@@ -50,8 +34,6 @@ class AutoMessage:
             time.sleep(5)
     
     def run(self):
-        self.data()
-        self.message_batch()
         self.message_send_wp()
 
 parser = argparse.ArgumentParser()
