24dfd5087fb95c037238312d1f5ea15c94acce37
mburakbilgic <muhammed.burak.bilgic@gmail.com>
2021-09-24 22:23:08 +0300

    feat(data_layer): data class implemented

---
+++ wp_automsg/wp_data/wp_data_resolver.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+class DataResolver:
+    def __init__(self):        
+        self.data_path = ('%s\phonebook.csv' %os.path.dirname(os.path.realpath(__file__))).replace('\\','/')
+        print(self.data_path)
+        self.arr_name = []
+        self.arr_pnumber = []
+
+    def _data(self):   
+        dataframe = pd.read_csv(self.data_path, sep=',',header=0)
+
+        for idx in dataframe.index:
+            pnumber = dataframe.loc[idx,'phonenumber']
+            name = dataframe.loc[idx,'name']
+            
+            self.arr_pnumber.append(str(pnumber))
+            self.arr_name.append(name)
