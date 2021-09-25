798088c1884b128ceae5396aacca3e8b40ef952a
mburakbilgic <muhammed.burak.bilgic@gmail.com>
2021-09-26 01:05:39 +0300

    feat(updates):unittest updated for new logics

--- test.py
+++ test.py
@@ -1,28 +1,30 @@
 import unittest
 import re
-from wp_automsg import wp_automsg_script as wpas
+import argparse
+
+from wp_automsg.wp_data.wp_data_resolver import DataResolver
+from wp_automsg.wp_batch.wp_message_batch import MessageBatch
 
 class TestEnvrinonment(unittest.TestCase):
 
     def testData(self):
-        expression_1 = len(wpas.AutoMessage()._data)
-        expression_2 = wpas.AutoMessage()._data
+        expression_2,expression_3 = DataResolver()._data()
 
-        self.assertIsNotNone(expression_1,"phonebook.csv should not empty")
-        
-        pnumber_pattern = r'^5[0-9][0-9][1-9]([0-9]){2}([0-9]){4}$'
-        
-        for idx in expression_2.index:
-            pnumber = expression_2.loc[idx,'phonenumber']
-            match = re.search(pnumber_pattern, str(pnumber))
+        pnumber_pattern = r'^5[0-9][0-9][1-9]([0-9]){2}([0-9]){4}$' 
+
+        for idx in expression_2: 
+            match = re.search(pnumber_pattern, str(idx))
             self.assertIsNotNone(match,"pnumber should be valid")
         
-        #TODO:multiple phonenumber
+        self.assertIsNotNone(expression_3,"contact name should be valid")
     
-    def testInput(self):
-        #TODO:input needs to be define
-        pass
+    def run(self):
+        self.testData()
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-t',help='Test')
+args = parser.parse_args()
 
-if __name__ == "__main__":
-    unittest.main()
+if args.t == 'test':
+    unit_test = TestEnvrinonment()
+    unit_test.run()
